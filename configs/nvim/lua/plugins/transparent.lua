22a6bfe4aed86f8c4a9423264aebb82e53d465d1
Gustavo Inácio <gsinacio94@gmail.com>
2024-06-11 13:15:19 -0300

    removes transparency, adds neotree config from example on git

--- configs/nvim/lua/plugins/transparent.lua
+++ configs/nvim/lua/plugins/transparent.lua
@@ -1,7 +1,6 @@
 return {
-  "xiyaowong/transparent.nvim",
-  config = function ()
-    vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
-  end
+  -- "xiyaowong/transparent.nvim",
+  -- config = function ()
+  --   vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
+  -- end
 }
-
