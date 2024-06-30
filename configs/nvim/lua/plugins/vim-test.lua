c33f1157624f56904180fc1c94001443ab7f4b9f
Gustavo Inácio <gsinacio94@gmail.com>
2024-06-30 15:48:07 -0300

    removes unused configs

--- configs/nvim/lua/plugins/vim-test.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vim-test/vim-test",
-  dependencies = { "preservim/vimux" },
-  vim.keymap.set("n", "<leader>tt", ":TestFile<CR>"),
-  vim.keymap.set("n", "<leader>a", ":TestSuite<CR>"),
-  vim.keymap.set("n", "<leader>l", ":TestLast<CR>"),
-  vim.keymap.set("n", "<leader>g", ":TestVisit<CR>"),
-  vim.cmd("let test#strategy='vimux'"),
-}
